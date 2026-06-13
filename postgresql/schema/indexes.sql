449d8d5a4506c38beabff017f7b78d9716199c0e
Jorge Díaz <joredudiaz@gmail.com>
2026-06-13 18:51:36 -0500

    Update indexes.sql

--- postgresql/schema/indexes.sql
+++ postgresql/schema/indexes.sql
@@ -1,6 +1,8 @@
 -- Reporte Mensual de Ventas
 CREATE INDEX idx_orders_purchase_month ON orders ((DATE_TRUNC('month', purchase_timestamp)::DATE));
 CREATE INDEX idx_order_items_composite ON order_items (order_id, created_at);
+CREATE INDEX ON public.orders USING btree (order_status_id);
+CREATE INDEX ON public.order_items USING btree (order_id);
 
 --  Perfil transaccional en el Checkout
 CREATE INDEX idx_orders_customer_id ON orders (customer_identification_number);
